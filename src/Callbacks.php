7c452bc171d23e2d211a20b1ee5c07c1b9e93eb7
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-04-07 16:36:38 +0200

    update namespace

--- src/Callbacks.php
+++ src/Callbacks.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 namespace PSX\OpenAPI;
 
 /**
- * @extends \PSX\Record\Record<Callback>
+ * @extends \PSX\Record\Record<\PSX\OpenAPI\Callback>
  */
 class Callbacks extends \PSX\Record\Record
 {
